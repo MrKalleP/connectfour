2ad2e78fb09d0b12c43eac168b69451ef469901a
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-08-23 12:26:14 +0200

    I have updated the type declaration

--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -1,10 +1,19 @@
 export default class Board {
   grid: string[][];
+  currentPlayerColor: string;
+  winner: boolean;
+  isADraw: boolean;
+  gameOver: boolean;
 
   constructor() {
     this.grid = Array.from({ length: 7 }, () =>
       Array.from({ length: 6 }, () => " ")
     );
+
+    this.currentPlayerColor = "X";
+    this.winner = false;
+    this.isADraw = false;
+    this.gameOver = false;
   }
 
   render() {
